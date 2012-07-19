ec6b8bbbeb72acfa9f4753137083e4768e9d7af7
GSSxGSS <gsshcl@gmail.com>
2012-07-19 21:10:58 +0800

    carousel done

--- js/global.js
+++ js/global.js
@@ -3,15 +3,29 @@ $(function(){
 });
 
 function displayCarousel(){
+  $('.thumbs').children().each(function(i) {
+    $(this).addClass( 'item'+i );
+    $(this).children('a').click(function() {
+      $('#carousel').trigger( 'slideTo', [i, 0, true] );
+      return false;
+    });
+  });
+  
   $('#carousel').carouFredSel({
+    direction: 'up',
+    circular: false,
+    infinite: false,
     auto    : false,
-    prev    : {
-      button  : "#carousel-prev",
-      key     : "left"
-    },
-    next    : {
-      button  : "#carousel-next",
-      key     : "right"
-    }
+    items   : 1,
+    prev: '#carousel-prev',
+    next: '#carousel-next',
+    scroll: {
+      fx: 'directscroll',
+      onBefore: function() {
+        var pos = $(this).triggerHandler('currentPosition');
+        $('#web-thumbs .work-box').removeClass('selected');
+        $('#web-thumbs .work-box.item'+pos).addClass('selected');
+      }
+    }    
   });
 }
